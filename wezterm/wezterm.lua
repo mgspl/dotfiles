0ea563f530ed72ed579535a622fadf9ac2e3fa39
Miguel <117107479+mgspl@users.noreply.github.com>
2024-04-20 23:22:10 +0000

    upd: wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,17 +9,9 @@ config.enable_wayland = false
 -- For example, changing the color scheme:
 config.color_scheme = 'Catppuccin Mocha'
 
-config.font = wezterm.font("NotoMonoNerdFont")
+config.font = wezterm.font("UbuntuMono Nerd Font")
 
-config.font_rules = {
-  {
-    intensity = 'Normal',
-    font=wezterm.font('NotoMonoNerdFont',
-    {weight="Regular", stretch='Normal', style=Normal}),
-  },
-}
-
-config.font_size = 13
+config.font_size = 14
 
 config.warn_about_missing_glyphs = false
 
@@ -27,6 +19,8 @@ config.prefer_to_spawn_tabs = true
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.detect_password_input = false
+config.detect_password_input = true
+
+config.default_prog = { 'bash' }
 
 return config
