82951bab2420d61d02ec2e09ebcaa512f44402bd
Miguel <117107479+mgspl@users.noreply.github.com>
2025-05-25 02:49:23 -0300

    update fonts

--- maomao/autostart.sh
+++ maomao/autostart.sh
@@ -1,16 +1,30 @@
 #! /bin/bash
 set +e
 
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+dbus-update-activation-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
 # The next line of command is not necessary. It is only to avoid some situations where it cannot start automatically
 /usr/lib/xdg-desktop-portal-wlr &
 
+pipewire &
 swaybg -i ~/.config/maomao/wallpaper.png &
 waybar &
 swaync &
 gammastep &
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/libexec/polkit-gnome-authentication-agent-1 &
 
 # KDE Connect replacement
-valent --gapplication-service &
+flatpak run ca.andyholmes.Valent --gapplication-service &
+
+
+# Idle
+swayidle \
+	timeout 1800 'swaylock -f' \
+	timeout 1805 'wlopm --off eDP-1 --off HDMI-A-1 ' \
+	resume 'wlopm --on eDP-1 --on HDMI-A-1' \
+	timeout 300 ' brightnessctl s 65%' \
+	resume 'brightnessctl s 100%' &
+
+sway-audio-idle-inhibit &
+
+swayosd-libinput-backend &
